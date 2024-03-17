38fabba11a2fcb45073870a7756693052d37f152
NghiaLOE <nghialoe46@gmail.com>
2024-03-17 16:05:04 +0700

    Update index env.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: process.env.REACT_APP_CLIENT,
+    origin: "https://pesterin.vercel.app",
     methods: ["GET","POST"]
   },
 });
