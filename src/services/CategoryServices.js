a88d0a7d8b2e96d78416503afe1228009b831d14
minhnd18-02 <lammjnphong4560@gmail.com>
2024-03-18 07:54:56 +0700

    clean code category

--- src/services/CategoryServices.js
+++ src/services/CategoryServices.js
@@ -2,53 +2,31 @@ import { Category } from "../models/Category.js";
 
 class CategoryService {
   async addNewCategories(models) {
-    try {
-      const categories = await Promise.all(
-        models.map(async (model) => {
-          const category = new Category(model);
-          await category.save();
-          return category;
-        })
-      );
-      return categories;
-    } catch (error) {
-      throw error;
-    }
+    const categories = await Promise.all(models.map(async (model) => {
+      const category = new Category(model);
+      await category.save();
+      return category;
+    }));
+    return categories;
   }
 
   async searchCategoryByName(partialName) {
-    try {
-      const categories = await Category.find(
-        { name: { $regex: new RegExp(partialName, "i") } },
-        { name: 1, _id: 0 }
-      );
-      const response = categories.map((category) => category.name);
-
-      return response;
-    } catch (error) {
-      throw new Error(`Error searching category: ${error.message}`);
-    }
+    const categories = await Category.find(
+      { name: { $regex: new RegExp(partialName, "i") } },
+      { name: 1, _id: 0 }
+    );
+    return categories.map((category) => category.name);
   }
 
   async getCategoryNames() {
-    try {
-      const categories = await Category.find({}, "name");
-      return categories.map((category) => ({
-        categoryName: category.name,
-        id: category._id,
-      }));
-    } catch (error) {
-      throw error;
-    }
+    return Category.find({}, "name").map((category) => ({
+      categoryName: category.name,
+      id: category._id,
+    }));
   }
 
   async getCategoryById(categoryId) {
-    try {
-      const categories = await Category.find({ _id: categoryId });
-      return categories;
-    } catch (error) {
-      throw error;
-    }
+    return Category.find({ _id: categoryId });
   }
 }
 
