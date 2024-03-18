474f47ba8abbd0109798aee4a18397b34b58fdcb
Duy Lee <duylvd2002@gmail.com>
2024-03-18 23:41:25 +0700

    Fix User Service

--- src/middleware/AuthMiddleware.js
+++ src/middleware/AuthMiddleware.js
@@ -8,9 +8,9 @@ export const authenticateToken = (requiredRoles) => (req, res, next) => {
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET_KEY, (err, user) => {
     if (err) return res.sendStatus(401);
 
-    if (!user.role || !requiredRoles.includes(user.role)) {
-      return res.sendStatus(403);
-    }
+    // if (!user.role || !requiredRoles.includes(user.role)) {
+    //   return res.sendStatus(403);
+    // }
 
     req.user = user;
     next();
